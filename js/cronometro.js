91e793195ae593d955eb5447ef23d673e22bf74c
Marcos Pablo <116852280+MarcosNVG@users.noreply.github.com>
2022-12-18 15:36:52 -0300

    funções basicas
    finalizei as funções básicas do relógio,(principalmente com relação ao cronometro e o timer)

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,57 +1,65 @@
 var bot = window.document.getElementById('butin')
+var mls = 0
 var sec = 0
 var min = 0
 var hora = 0
-ctempo.innerHTML = `${hora}:${min}:${sec}`
-
-function cronometrar(){
-   var tempo = setInterval(cronometro, 1000)
-        function cronometro(){
-            sec += 1
-            ctempo.innerHTML = `${hora}:${min}:${sec}`
-
-            if (sec == 59) {
-                min += 1
-                sec = -1
-            }
-        }
+var tempo
+ 
+//Interatividade com os botões
+function iniciar(){
+    cronometro()
+    tempo = setInterval(cronometro, 10)
+    dbt.innerHTML = '<input type="button" value="Parar" id="parin" onclick="parar()" >'
+        
 }
 
+function parar(){
+    clearInterval(tempo)
+    dbt.innerHTML = '<input type="button" value="Resetar" id="resin" onclick="reset()" >'
+}
 
+function reset(){
+    hora = 0
+    min = 0
+    sec = 0
+    mls = 0
+    ctempo.innerHTML = twoDigits(sec)+':'+twoDigits(mls)
+    dbt.innerHTML = '<input type="button" value="Iniciar" id="butin" onclick="iniciar()">'
+}
 
+//funcionalidades internas
 
+function cronometro(){
+    mls += 1
 
-//if(bot.value == "Iniciar"){
-
-
-
-/*var sec = 00
-var min = 00
-var hora = 00
-ctempo.innerHTML = `${hora}:${min}:${sec}`
-
-function cronometrar(){
-    setInterval(cronometro, 1000)
-    bot.value = "Parar" 
-    
-    function cronometro(){
-        sec += 1
-        ctempo.innerHTML = `${hora}:${min}:${sec}`
-        
-
-        if (sec == 59) {
-            min += 1
-            sec = -1
-        }
-
-        if (bot.value == "Parar"){
-            bot.addEventListener('click', parar)
-        }    
+    if (mls == 100){
+        sec++
+        mls = 0
+    }
+    if (sec == 60) {
+        min += 1
+        sec = 0
+    }  
+    if (min == 60 && sec == 00){
+        hora += 1
+        min = 0
+        sec = 0
     }
 
-    function parar(){
-        cronometrar.break
+    if (hora==0 && min == 0){
+        ctempo.innerHTML = twoDigits(sec)+':'+twoDigits(mls)
+    } else if (hora == 0) {
+        ctempo.innerHTML = twoDigits(min)+':'+twoDigits(sec)+':'+twoDigits(mls)
+    } else{
+        ctempo.innerHTML = twoDigits(hora)+':'+twoDigits(min)+':'+twoDigits(sec)+':'+twoDigits(mls)
     }
-}*/
+}
 
-close
+function twoDigits(Digits){
+    if (Digits < 10){
+        return('0'+ Digits)
+    } else {
+        return(Digits)
+    }
+}
+
--- js/timer.js
+++ js/timer.js
@@ -1,44 +1,86 @@
+var hora = window.document.getElementById('inhora');
+var min = window.document.getElementById('inmin');
+var seg = window.document.getElementById('inseg');
+var bot = window.document.getElementById('bt')
+var h
+var m
+var s
+var time 
+
+//interatividade com os botões
 function iniciar(){
-    var hora = window.document.getElementById('inhora');
-    var min = window.document.getElementById('inmin');
-    var seg = window.document.getElementById('inseg');
-    var h = hora.value;
-    var m = min.value;
-    var s = seg.value
-    if (hora.value.length == 0 ){
-        h = 0
-    }
-    if (min.value.length == 0 ){
-        m = 0
-    }
-    if (seg.value.length == 0 ){
-        s = 0
-    }
-    dsec.innerHTML = `${h}:${m}:${s}`
-
-    var time = setInterval(function(){
-        if (h >= 0 && s > -1 || m >= 0 && s >=0){
-            s-- 
-            dsec.innerHTML = `${h}:${m}:${s}`    
+    h = hora.value;
+    m = min.value;
+    s = seg.value
+    if (h == 0 && m == 0 && s == 0){
+        alert('Insira um tempo para começar.')
+        dsec.innerHTML = ""
+    } else {
+        if (hora.value.length == 0 ){
+            h = 0
         }
-        if (m > 0 && s == -1){
-            m--
-            s = 59
-            dsec.innerHTML = `${h}:${m}:${s}`
-        } else if (h > 0 && s == -1) {
-            s = 59 
-            m = 59
-            h -= 1
-            dsec.innerHTML = `${h}:${m}:${s}`  
-        }else if (h == 0 && m == 0 && s == 0){
-            alert("Timer")
-            clearInterval(time)
+        if (min.value.length == 0 ){
+            m = 0
         }
-    }, 1000)
+        if (seg.value.length == 0 ){
+            s = 0
+        }
+        write()
+        time = setInterval(timer, 1000) 
+        bot.innerHTML = '<input type="button" value="Parar" onclick="parar()">'
+        hora.value = ''
+    }
 }
+
 function parar(){
-    setTimeout(function(){clearInterval(time)}, 1000)
+    clearInterval(time)
+    bot.innerHTML = '<input type="button" value="Continue" onclick="continuar()">'
+    bot.innerHTML += '<input type="button" value="Iniciar" onclick="iniciar()">'
 }
 
+function continuar(){
+    time = setInterval(timer, 1000)
+    bot.innerHTML = '<input type="button" value="Parar" onclick="parar()">'
+}
+
+//funcionalidade do programa
+
+function timer(){
+    if (s >= 0){
+        s--    
+    }
+
+    if (m > 0 && s < 0){
+        m--
+        s = 59  
+    } else if (h > 0 && s < 0) {
+        s = 59 
+        m = 59
+        h --   
+    }else if (h == 0 && m == 0 && s == 0){
+        alert("Timer")
+        clearInterval(time) 
+    }
+
+   write()
+}
+
+function write(){
+    if(h == 0 && m == 0){
+        dsec.innerHTML =  twoDigits(s)
+    } else if (h == 0) {
+        dsec.innerHTML = twoDigits(m) +':'+ twoDigits(s)  
+    } else {
+        dsec.innerHTML = twoDigits(h) +':'+ twoDigits(m) +':'+ twoDigits(s)  
+    }
+}
+
+function twoDigits(digits){
+    if (digits < 10){
+        return ('0' + digits)
+    } else {
+        return (digits)
+    }
+}
 
 
